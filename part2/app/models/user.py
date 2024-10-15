5e50a7ac665ae0f1ba8f5937c34505fcce0f839d
jeje-digifab <j.romand.digifab@gmail.com>
2024-10-15 13:37:15 +0200

    Implement Place model with validation and methods for reviews and amenities

--- part2/app/models/user.py
+++
@@ -1,8 +0,0 @@
-# app/models/user.py
-
-class User:
-    def __init__(self, first_name, last_name, email, is_admin=False):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.is_admin = is_admin
