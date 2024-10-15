f721d90d2f0f20493ae39e376af5ab990d575c1b
jeje-digifab <j.romand.digifab@gmail.com>
2024-10-15 15:22:59 +0200

    Adjust basemodel.py for pycodestyle compliance

--- part2/app/models/BaseModel.py
+++ part2/app/models/BaseModel.py
@@ -4,19 +4,22 @@ from datetime import datetime
 
 class BaseModel:
     """
-    A base model class that provides unique identification and timestamping for instances.
+    A base model class that provides unique identification and
+    timestamping for instances.
 
     Attributes:
         id (str): A unique identifier for the instance.
         created_at (datetime): The timestamp when the instance was created.
-        updated_at (datetime): The timestamp when the instance was last updated.
+        updated_at (datetime): The timestamp when the instance
+        was last updated.
     """
 
     def __init__(self):
         """
         Initializes a new instance of BaseModel.
 
-        Generates a unique id using UUID and sets created_at and updated_at timestamps to the current datetime.
+        Generates a unique id using UUID and sets created_at and updated_at
+        timestamps to the current datetime.
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
@@ -26,7 +29,8 @@ class BaseModel:
         """
         Updates the updated_at timestamp whenever the object is modified.
 
-        This method should be called after any changes to the object's attributes.
+        This method should be called after any changes
+        to the object's attributes.
         """
         self.updated_at = datetime.now()
 
@@ -35,9 +39,11 @@ class BaseModel:
         Updates the attributes of the object based on the provided dictionary.
 
         Args:
-            data (dict): A dictionary containing attribute names and their new values.
+            data (dict): A dictionary containing attribute names and
+            their new values.
 
-        Updates only the attributes that exist in the object, and calls save() to update the timestamp.
+        Updates only the attributes that exist in the object, and calls save()
+        to update the timestamp.
         """
         for key, value in data.items():
             if hasattr(self, key):
@@ -49,7 +55,8 @@ class BaseModel:
         Returns a string representation of the object for debugging.
 
         Returns:
-            str: A formatted string representing the object, including class name, id, created_at, and updated_at timestamps.
+            str: A formatted string representing the object,
+            including class name, id, created_at, and updated_at timestamps.
         """
         return (
             f"<{self.__class__.__name__}(id={self.id}, "
