66f5720f7783d44b708057df3a6e3b0a9280557c
zyclamenace <gemicieneshakki@gmail.com>
2024-10-18 15:35:42 +0200

    Fix: pycodestyle

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -11,7 +11,7 @@ def create_app():
     """app configuration"""
     app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API',
-              description='HBnB Application API')
+            description='HBnB Application API')
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
     app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
     app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
