eb74dff7aceaf6ea35f859c60286042657ab1ddd
NadegeL <nadege.luthier@gmai.com>
2024-10-20 11:24:20 +0200

    essais users

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_restx import Api
 from flask import Flask
 from flask_jwt_extended import JWTManager
+from app.api.v1.users import api as users_ns
+from app.api.v1.auth import api as auth_ns
 import os
 from dotenv import load_dotenv
 
@@ -12,19 +14,21 @@ def create_app():
     app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API',
               description='HBnB Application API')
-    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config['SECRET_KEY'] = 'your secret key'
     app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
     app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
     
     jwt = JWTManager(app)
     
-    from app.api.v1 import users, places, reviews, amenities
+    api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(auth_ns, path='/api/v1/auth')
+    
+    return app
+    
+    """from app.api.v1 import users, places, reviews, amenities
     app.register_blueprint(users.bp)
     app.register_blueprint(places.bp)
     app.register_blueprint(reviews.bp)
-    app.register_blueprint(amenities.bp)
-    
-    return app
-
+    app.register_blueprint(amenities.bp)"""
     # Placeholder for API namespaces (endpoints will be added later)
     # Additional namespaces for places, reviews, and amenities will be added later
--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -8,10 +8,34 @@ class HBnBFacade:
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
-    # Placeholder method for creating a user
     def create_user(self, user_data):
-        # Logic will be implemented in later tasks
-        pass
+        user = user(**user_data)
+        self.user_repo.add(user)
+        return user
+
+    def get_user(self, user_id):
+        return self.user_repo.get(user_id)
+
+    def get_all_users(self):
+        return self.user_repo.get_all()
+
+    def get_user_by_email(self, email):
+        return self.user_repo.get_by_attribute('email', email)
+
+    def update_user(self, user_id, user_data):
+        user = self.get_user(user_id)
+        if user:
+            for key, value in user_data.items():
+                setattr(user, key, value)
+            self.user_repo.update(user_id, user_data)
+            return user
+        return None
+    
+    def authenticate_user(self, email, password):
+        user = self.user_repo.get_by_attribute('email', email)
+        if user and user.check_password(password):
+            return user
+        return None
 
     # Placeholder method for fetching a place by ID
     def get_place(self, place_id):
