5757a31a1914b82f6e683066e2ad5de1cd069554
zyclamenace <gemicieneshakki@gmail.com>
2024-10-21 11:31:39 +0200

    Update: Remove Blueprint

--- part2/app/api/v1/amenities.py
+++ part2/app/api/v1/amenities.py
@@ -1,7 +1,6 @@
-from flask import Flask, jsonify, request, abort, Blueprint
+from flask import Flask, jsonify, request, abort
 from app.models.amenity import Amenity
 app = Flask(__name__)
-bp = Blueprint('amenities', __name__)
 
 # list to stock Amenities
 amenities = []
