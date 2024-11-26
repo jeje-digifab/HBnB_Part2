b3b9e5df5c3066dde7a33be51ef2d67988217c9e
ZykeLadebrouile <gemicieneshakki@gmail.com>
2024-11-26 13:51:15 +0100

    Implement CORS support and refactor login functionality in scripts.js (deleted login.js)

--- part4/scripts.js
+++ part4/scripts.js
@@ -24,4 +24,40 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
+// login-loader
+document.addEventListener('DOMContentLoaded', () => {
+  const loginForm = document.getElementById('login-form');
+
+  if (loginForm) {
+  loginForm.addEventListener('submit', async (event) => {
+      event.preventDefault();
+      
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
+
+      try {  // modify fetch url if necessary
+          const response = await fetch('http://localhost:5000/api/v1/auth/login', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({ email, password })
+          });
+
+          if (response.ok) {
+              const data = await response.json();
+              document.cookie = `token=${data.access_token}; path=/; max-age=3600; SameSite=Strict`;
+              window.location.href = 'index.html';
+          } else {
+              const errorData = await response.json();
+              alert('Login failed : ' + (errorData.message || response.statusText));
+          }
+      } catch (error) {
+          console.error('Login failed:', error);
+          alert('An error occurred while logging in');
+      }
+  });
+  }
+});
+
 
