61894f34cae0fab6b3f2978b418d9d972993a142
NadegeL <nadege.luthier@gmai.com>
2024-11-26 22:29:23 +0100

    place.html à jour, Css à jour, JS modifié

--- part4/scripts.js
+++ part4/scripts.js
@@ -29,34 +29,34 @@ document.addEventListener('DOMContentLoaded', () => {
   const loginForm = document.getElementById('login-form');
 
   if (loginForm) {
-  loginForm.addEventListener('submit', async (event) => {
+    loginForm.addEventListener('submit', async (event) => {
       event.preventDefault();
-      
+
       const email = document.getElementById('email').value;
       const password = document.getElementById('password').value;
 
       try {  // modify fetch url if necessary
-          const response = await fetch('http://localhost:5000/api/v1/auth/login', {
-              method: 'POST',
-              headers: {
-                  'Content-Type': 'application/json'
-              },
-              body: JSON.stringify({ email, password })
-          });
+        const response = await fetch('http://localhost:5000/api/v1/auth/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ email, password })
+        });
 
-          if (response.ok) {
-              const data = await response.json();
-              document.cookie = `token=${data.access_token}; path=/; max-age=3600; SameSite=Strict`;
-              window.location.href = 'index.html';
-          } else {
-              const errorData = await response.json();
-              alert('Login failed : ' + (errorData.message || response.statusText));
-          }
+        if (response.ok) {
+          const data = await response.json();
+          document.cookie = `token=${data.access_token}; path=/; max-age=3600; SameSite=Strict`;
+          window.location.href = 'index.html';
+        } else {
+          const errorData = await response.json();
+          alert('Login failed : ' + (errorData.message || response.statusText));
+        }
       } catch (error) {
-          console.error('Login failed:', error);
-          alert('An error occurred while logging in');
+        console.error('Login failed:', error);
+        alert('An error occurred while logging in');
       }
-  });
+    });
   }
 });
 
@@ -90,6 +90,16 @@ document.addEventListener('DOMContentLoaded', function () {
     .catch(error => console.error('Error loading cards:', error));
 });
 
+//change rating note in stars
+const ratingLinks = document.querySelectorAll('.rating a');
+
+ratingLinks.forEach(link => {
+  link.addEventListener('click', function (event) {
+    event.preventDefault(); // Empêche le comportement par défaut du lien
+    const ratingValue = this.getAttribute('href').substring(1); // Récupère la valeur de l'étoile
+    console.log(`${ratingValue} stars!`); // Affiche la note dans la console ou utilisez-la comme bon vous semble
+  });
+});
 //Check user authentication
 function checkAuthentication() {
   const token = getCookie('token');
