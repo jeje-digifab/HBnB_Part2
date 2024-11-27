0f4d43cc60fa18e94a1f2a61ff549b98966716b0
zyclamenace <gemicieneshakki@gmail.com>
2024-11-27 10:43:45 +0100

    Update: GetCookie and Checkauthenticate already included

--- part4/scripts.js
+++ part4/scripts.js
@@ -120,23 +120,7 @@ ratingLinks.forEach(link => {
     console.log(`${ratingValue} stars!`); // Affiche la note dans la console ou utilisez-la comme bon vous semble
   });
 });
-//Check user authentication
-function checkAuthentication() {
-  const token = getCookie('token');
-  const loginLink = document.getElementById('login-link');
 
-  if (!token) {
-    loginLink.style.display = 'block';
-  } else {
-    loginLink.style.display = 'none';
-    // Fetch places data if the user is authenticated
-    fetchPlaces(token);
-  }
-}
-function getCookie(name) {
-  // Function to get a cookie value by its name
-  // Your code here
-}
 
 //Fetch places data
 async function fetchPlaces(token) {
